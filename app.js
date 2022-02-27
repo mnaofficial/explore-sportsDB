3b90696b631c4d811e9363ab72d4e61a6487b929
mnaofficial <mna.official92@gmail.com>
2022-02-28 00:18:22 +0600

    thirt commit

--- app.js
+++ app.js
@@ -7,13 +7,29 @@ https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}
     // console.log(url)
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.player))
+        .then(data => showPlayerDetails(data.player))
+};
+const showPlayerDetails = (players) => {
+    for (const player of players) {
 
+        const parent = document.getElementById('player-container');
+        const div = document.createElement('div');
+        div.innerHTML = `
+       <div class="card border">
+          <div class="pro-pick">
+             <img class="w-25" src="" alt="">
+          </div>
+            <h2>Name</h2>
+            <h5>Country</h5>
+            <p></p>
+         <div class="allbutton">
+           <button class="btn btn-danger">Delete</button>
+           <button class="btn btn-success">Details</button>
+         </div>
+      </div>
+    `;
+        parent.appendChild(div);
 
-    // searchValue.value = ' ';
-    // console.log(searchValue);
-
+        console.log(player);
+    };
 }
-
-// const newtext = searchBox.value;
-// console.log(newtext);
